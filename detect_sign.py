f390966733edcf55a9e871a3ea97e61de9c5ad15
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-06-03 09:55:42 +0700

    Fixing filter

--- detect_sign.py
+++ detect_sign.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import imutils
+import math
+import functools
+
 
 def identify_red(imag):
     img = imag.copy()
@@ -18,7 +21,7 @@ def identify_red(imag):
     lower_red_1 = np.array([0, 70, 60])
     upper_red_1 = np.array([10, 255, 255])
     mask_1 = cv2.inRange(img_hsv, lower_red_1, upper_red_1)
-    lower_red_2 = np.array([170, 70, 60])
+    lower_red_2 = np.array([170, 50, 50])
     upper_red_2 = np.array([180, 255, 255])
     mask_2 = cv2.inRange(img_hsv, lower_red_2, upper_red_2)
     mask = cv2.bitwise_or(mask_1, mask_2)
@@ -34,7 +37,7 @@ def identify_red(imag):
     filtered_g = cv2.medianBlur(g_channel, 5)
     filtered_b = cv2.medianBlur(b_channel, 5)
 
-    filtered_r = 4 * filtered_r - 0.5 * filtered_b - 2 * filtered_g
+    filtered_r = 6 * filtered_r - 0.5 * filtered_b - 2 * filtered_g
 
     blank = np.uint8(filtered_r)
 
@@ -44,19 +47,22 @@ def identify_red(imag):
     erosion = cv2.erode(blank, kernel_1, iterations=1)
     dilation = cv2.dilate(erosion, kernel_2, iterations=1)
     opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel_2)
-    _, r_thresh = cv2.threshold(opening, 20, 255, cv2.THRESH_BINARY)
+    _, r_thresh = cv2.threshold(opening, 10, 255, cv2.THRESH_BINARY)
 
     cnts = cv2.findContours(r_thresh, cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    if not cnts == []:
-        cnts_sorted = sorted(cnts, key=cv2.contourArea, reverse=True)
-        # c = cnts_sorted[0]
-        # x, y, w, h = cv2.boundingRect(c)
-        # cv2.rectangle(imag, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
-        return cnts_sorted
-    else:
-        return None
+
+    return cnts
+
+    # if not cnts == []:
+    #     cnts_sorted = sorted(cnts, key=cv2.contourArea, reverse=True)
+    #     # c = cnts_sorted[0]
+    #     # x, y, w, h = cv2.boundingRect(c)
+    #     # cv2.rectangle(imag, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
+    #     return cnts_sorted
+    # else:
+    #     return None
 
 
 def identify_blue(imag):
@@ -73,7 +79,7 @@ def identify_blue(imag):
     img_hsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV)
 
     # mask to extract blue
-    lower_blue = np.array([94, 127, 20])
+    lower_blue = np.array([96, 127, 20])
     upper_blue = np.array([126, 255, 200])
     mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
 
@@ -90,7 +96,7 @@ def identify_blue(imag):
     filtered_b = cv2.medianBlur(b_channel, 5)
 
     # create a blue gray space
-    filtered_b = -0.5 * filtered_r + 3 * filtered_b - 2 * filtered_g
+    filtered_b = -0.5 * filtered_r + 4 * filtered_b - 0.5 * filtered_g
 
     blank = np.uint8(filtered_b)
 
@@ -106,14 +112,18 @@ def identify_blue(imag):
     cnts = cv2.findContours(b_thresh, cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    if not cnts == []:
-        cnts_sorted = sorted(cnts, key=cv2.contourArea, reverse=True)
-        # c = cnts_sorted[0]
-        # x, y, w, h = cv2.boundingRect(c)
-        # cv2.rectangle(imag, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
-        return cnts_sorted
-    else:
-        return None
+
+    return cnts
+
+    # if not cnts == []:
+    #     cnts_sorted = sorted(cnts, key=cv2.contourArea, reverse=True)
+    #     # c = cnts_sorted[0]
+    #     # x, y, w, h = cv2.boundingRect(c)
+    #     # cv2.rectangle(imag, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
+    #     return cnts_sorted
+    # else:
+    #     return None
+
 
 def identify_yellow(imag):
     img = imag.copy()
@@ -129,11 +139,10 @@ def identify_yellow(imag):
     img_hsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV)
 
     # mask to extract blue
-    lower_yellow = np.array([20, 100, 100])
-    upper_yellow = np.array([30, 255, 255])
+    lower_yellow = np.array([15, 100, 100])
+    upper_yellow = np.array([40, 255, 255])
     mask = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
-
-    yellow_mask = cv2.bitwise_and(img_output, img_output, mask=mask)
+    yellow_mask = cv2.bitwise_or(img_hsv, img_hsv, mask=mask)
 
     # seperate out the channels
     r_channel = yellow_mask[:, :, 2]
@@ -146,11 +155,9 @@ def identify_yellow(imag):
     filtered_b = cv2.medianBlur(b_channel, 5)
 
     # create a yellow gray space
-    filtered_b = -0.5 * filtered_r + 3 * filtered_b + 3 * filtered_g
+    filtered_b = 3 * filtered_r - 0.5 * filtered_b + 5 * filtered_g
 
     blank = np.uint8(filtered_b)
-
-    cv2.imshow("y", blank)
 
     kernel_1 = np.ones((3, 3), np.uint8)
     kernel_2 = np.ones((5, 5), np.uint8)
@@ -159,43 +166,99 @@ def identify_yellow(imag):
     dilation = cv2.dilate(erosion, kernel_2, iterations=1)
     opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel_2)
 
-    _, b_thresh = cv2.threshold(opening, 20, 255, cv2.THRESH_BINARY)
+    _, b_thresh = cv2.threshold(opening, 50, 255, cv2.THRESH_BINARY)
 
     cnts = cv2.findContours(b_thresh, cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    if not cnts == []:
-        cnts_sorted = sorted(cnts, key=cv2.contourArea, reverse=True)
-        c = cnts_sorted[0]
-        # x, y, w, h = cv2.boundingRect(c)
-        # cv2.rectangle(imag, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
-        return cnts_sorted
+
+    return cnts
+
+    # if not cnts == []:
+    #     cnts_sorted = sorted(cnts, key=cv2.contourArea, reverse=True)
+    #     c = cnts_sorted[0]
+    #     # x, y, w, h = cv2.boundingRect(c)
+    #     # cv2.rectangle(imag, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
+    #     return cnts_sorted
+    # else:
+    #     return None
+
+
+def contourComparator(c1, c2):
+    a1 = cv2.contourArea(c1)
+    a2 = cv2.contourArea(c2)
+    _, _, w1, _ = cv2.boundingRect(c1)
+    _, _, w2, _ = cv2.boundingRect(c2)
+    r1 = w1 / 2
+    r2 = w2 / 2
+    approx_area_1 = math.pi * r1 * r1
+    approx_area_2 = math.pi * r2 * r2
+    ratio_1 = a1 / approx_area_1
+    ratio_2 = a2 / approx_area_2
+
+    min_ratio_threshold = 0.9
+    max_ratio_threshold = 1.1
+
+    is_in_ratio_1 = ratio_1 >= min_ratio_threshold and ratio_1 <= max_ratio_threshold
+    is_in_ratio_2 = ratio_2 >= min_ratio_threshold and ratio_2 <= max_ratio_threshold
+
+    if a1 / a2 >= min_ratio_threshold and a1 / a2 <= max_ratio_threshold:
+        # Prioritize circle if detected to eliminate noise
+        if is_in_ratio_1 and not is_in_ratio_2:
+            return 1
+        elif is_in_ratio_2 and not is_in_ratio_1:
+            return -1
+        else:
+            return 0
     else:
-        return None
+        return a1 - a2
+
 
 while True:
-    img = cv2.imread('test_images/red_8.jpeg')
+    img = cv2.imread('test_images/blue_1.jpg')
 
     red_list = identify_red(img)
     blue_list = identify_blue(img)
     yellow_list = identify_yellow(img)
 
-    biggest_area_candidates = []
-    if red_list:
-        biggest_area_candidates.append(red_list[0])
-    if blue_list:
-        biggest_area_candidates.append(blue_list[0])
-    if yellow_list:
-        biggest_area_candidates.append(yellow_list[0])
+    candidates = red_list + blue_list + yellow_list
 
-    biggest_area_candidates = sorted(biggest_area_candidates,
-                          key=cv2.contourArea, reverse=True)
+    # candidates = sorted(
+    #     candidates, key=functools.cmp_to_key(contourComparator), reverse=True)
 
-    if len(biggest_area_candidates) > 0:
-        c = biggest_area_candidates[0]
+    candidates = sorted(
+        candidates, key=cv2.contourArea, reverse=True)
+
+    found = False
+
+    # Check if aspect ratio match
+    min_aspect_ratio = 0.9
+    max_aspect_ratio = 1.1
+
+    for c in candidates:
         x, y, w, h = cv2.boundingRect(c)
-        cv2.rectangle(img, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
-    else:
+        aspect_ratio = w / h
+
+        if aspect_ratio >= min_aspect_ratio and aspect_ratio <= max_aspect_ratio:
+            cv2.rectangle(img, (x, y), (int(x + w),
+                                        int(y + h)), (0, 255, 0), 2)
+            found = True
+            break
+
+    x, y, w, h = cv2.boundingRect(candidates[0])
+    r = w / 2
+    a = cv2.contourArea(candidates[0])
+
+    # cv2.drawContours(img, [candidates[2]], 0,
+    #                  (0, 255, 0), thickness=cv2.FILLED)
+
+    aa = math.pi * r * r
+    aspect_ratio = w / h
+    print(aspect_ratio, w, h, a, aa, w * h)
+    cv2.rectangle(img, (x, y), (int(x + w),
+                                int(y + h)), (0, 255, 0), 2)
+
+    if not found:
         print("No traffic sign found!")
 
     cv2.imshow("Result", img)
