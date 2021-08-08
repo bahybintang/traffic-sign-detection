bce3bcaec325239bf2e9fe1a70993a908ce9880a
Muhammad Bintang Bahy <bahybintang@gmail.com>
2021-08-08 19:37:54 +0700

    Update detector sign by color and test images

--- detect_sign.py
+++ detect_sign.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import cv2
 import numpy as np
 import imutils
@@ -5,10 +6,11 @@ import math
 import functools
 import matplotlib.pyplot as plt
 import os
+import sys
 
-CATEGORY_PERINGATAN = 1
-CATEGORY_LARANGAN = 2
-CATEGORY_PERINTAH = 3
+CATEGORY_PERINGATAN = '1'
+CATEGORY_LARANGAN = '2'
+CATEGORY_PERINTAH = '3'
 
 
 def identify_red(imag):
@@ -51,7 +53,7 @@ def identify_red(imag):
     kernel_2 = np.ones((5, 5), np.uint8)
 
     erosion = cv2.erode(blank, kernel_1, iterations=1)
-    dilation = cv2.dilate(erosion, kernel_2, iterations=1)
+    dilation = cv2.dilate(erosion, kernel_2, iterations=3)
     opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel_2)
     _, r_thresh = cv2.threshold(opening, 10, 255, cv2.THRESH_BINARY)
 
@@ -110,10 +112,10 @@ def identify_blue(imag):
     kernel_2 = np.ones((5, 5), np.uint8)
 
     erosion = cv2.erode(blank, kernel_1, iterations=1)
-    dilation = cv2.dilate(erosion, kernel_2, iterations=1)
+    dilation = cv2.dilate(erosion, kernel_2, iterations=3)
     opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel_2)
 
-    _, b_thresh = cv2.threshold(opening, 60, 255, cv2.THRESH_BINARY)
+    _, b_thresh = cv2.threshold(opening, 10, 255, cv2.THRESH_BINARY)
 
     cnts = cv2.findContours(b_thresh, cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
@@ -170,12 +172,12 @@ def identify_yellow(imag):
     kernel_2 = np.ones((5, 5), np.uint8)
 
     erosion = cv2.erode(blank, kernel_1, iterations=1)
-    dilation = cv2.dilate(erosion, kernel_2, iterations=1)
+    dilation = cv2.dilate(erosion, kernel_2, iterations=3)
     opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel_2)
 
-    _, b_thresh = cv2.threshold(opening, 30, 255, cv2.THRESH_BINARY)
+    _, y_thresh = cv2.threshold(opening, 10, 255, cv2.THRESH_BINARY)
 
-    cnts = cv2.findContours(b_thresh, cv2.RETR_EXTERNAL,
+    cnts = cv2.findContours(y_thresh, cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
@@ -223,6 +225,7 @@ def contourComparator(c1, c2):
 
 def detect_sign(img, is_yolo=False, category=False):
     global CATEGORY_PERINGATAN, CATEGORY_LARANGAN, CATEGORY_PERINTAH
+    category = str(category)
 
     img_height, img_width, _ = img.shape
 
@@ -264,53 +267,105 @@ def detect_sign(img, is_yolo=False, category=False):
 def detect_sign_by_path(path, is_yolo=False, category=False):
     return detect_sign(cv2.imread(path), is_yolo, category)
 
-# while True:
-#     img = cv2.imread('test_images/blue_1.jpg')
 
-#     red_list = identify_red(img)
-#     blue_list = identify_blue(img)
-#     yellow_list = identify_yellow(img)
+def imShow(img, label="Image"):
+    img = cv2.resize(img, (int(img.shape[1] * 900 / img.shape[0]), 900))
+    cv2.imshow(label, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-#     candidates = red_list + blue_list + yellow_list
 
-#     # candidates = sorted(
-#     #     candidates, key=functools.cmp_to_key(contourComparator), reverse=True)
+def show_image_with_boundary(img, boundary):
+    x, y, w, h = boundary
+    img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    imShow(img, "Hasil deteksi")
 
-#     candidates = sorted(
-#         candidates, key=cv2.contourArea, reverse=True)
 
-#     found = False
+def show_hough(image):
+    # Process red
+    img = image.copy()
 
-#     # Check if aspect ratio match
-#     min_aspect_ratio = 0.9
-#     max_aspect_ratio = 1.1
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 
-#     for c in candidates:
-#         x, y, w, h = cv2.boundingRect(c)
-#         aspect_ratio = w / h
+    # equalize the histogram of the Y channel
+    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
 
-#         if aspect_ratio >= min_aspect_ratio and aspect_ratio <= max_aspect_ratio:
-#             cv2.rectangle(img, (x, y), (int(x + w),
-#                                         int(y + h)), (0, 255, 0), 2)
-#             found = True
-#             break
+    # convert the YUV image back to RGB format
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 
-#     # x, y, w, h = cv2.boundingRect(candidates[0])
-#     # r = w / 2
-#     # a = cv2.contourArea(candidates[0])
+    # mask to extract red
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_red_1 = np.array([0, 70, 60])
+    upper_red_1 = np.array([10, 255, 255])
+    mask_1 = cv2.inRange(img_hsv, lower_red_1, upper_red_1)
+    lower_red_2 = np.array([170, 50, 50])
+    upper_red_2 = np.array([180, 255, 255])
+    mask_2 = cv2.inRange(img_hsv, lower_red_2, upper_red_2)
+    mask = cv2.bitwise_or(mask_1, mask_2)
+    red_mask = cv2.bitwise_and(img_output, img_output, mask=mask)
 
-#     # # cv2.drawContours(img, [candidates[2]], 0,
-#     # #                  (0, 255, 0), thickness=cv2.FILLED)
+    # separating channels
+    r_channel = red_mask[:, :, 2]
+    g_channel = red_mask[:, :, 1]
+    b_channel = red_mask[:, :, 0]
 
-#     # aa = math.pi * r * r
-#     # aspect_ratio = w / h
-#     # print(aspect_ratio, w, h, a, aa, w * h)
-#     # cv2.rectangle(img, (x, y), (int(x + w),
-#     #                             int(y + h)), (0, 255, 0), 2)
+    # filtering
+    filtered_r = cv2.medianBlur(r_channel, 5)
+    filtered_g = cv2.medianBlur(g_channel, 5)
+    filtered_b = cv2.medianBlur(b_channel, 5)
 
-#     if not found:
-#         print("No traffic sign found!")
+    filtered_r = 6 * filtered_r - 0.5 * filtered_b - 2 * filtered_g
 
-#     cv2.imshow("Result", img)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+    blank = np.uint8(filtered_r)
+
+    kernel_1 = np.ones((3, 3), np.uint8)
+    kernel_2 = np.ones((5, 5), np.uint8)
+
+    erosion = cv2.erode(blank, kernel_1, iterations=1)
+    dilation = cv2.dilate(erosion, kernel_2, iterations=4)
+    opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel_2)
+    _, r_thresh = cv2.threshold(opening, 10, 255, cv2.THRESH_BINARY)
+
+    cnts = cv2.findContours(r_thresh, cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    for c in cnts:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (int(x + w), int(y + h)), (0, 255, 0), 2)
+
+    imShow(image)
+
+    output = image.copy()
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    imShow(r_thresh)
+    imShow(blank)
+    imShow(dilation)
+    imShow(erosion)
+
+    # detect circles in the image
+    circles = cv2.HoughCircles(r_thresh, cv2.HOUGH_GRADIENT, 1.2, 100)
+
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(output, (x - 5, y - 5),
+                          (x + 5, y + 5), (0, 128, 255), -1)
+        # show the output image
+        cv2.imshow("output", np.hstack([image, output]))
+        cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    boundary = detect_sign_by_path(
+        sys.argv[1], is_yolo=False, category=sys.argv[2])
+    show_image_with_boundary(cv2.imread(sys.argv[1]), boundary)
+    # identify_red(cv2.imread(sys.argv[1]))
+    # show_hough(cv2.imread(sys.argv[1]))
+    exit()
